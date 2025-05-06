21583db8c773d548f95f02e2ac705ba5b3a4bad0
brweinstein <brweinstein68@gmail.com>
2025-05-06 13:59:46 -0400

    updated nvim, tmux, and alacritty

---
+++ nvim/init.lua
@@ -0,0 +1,10 @@
+require("core.keymaps")
+require("core.plugins")
+require("core.plugin_config")
+
+vim.cmd("set autoindent")
+vim.cmd("set number")
+vim.opt.relativenumber = true
+vim.o.shiftwidth = 4
+vim.cmd("set noexpandtab")
+vim.o.tabstop = 4
